16435da581790d0d2ab23f3539e5018fc3ff690d
emilyh1013 <138421980+emilyh1013@users.noreply.github.com>
2025-10-30 01:01:03 +0000

    first

--- hi.py
+++ hi.py
@@ -1,1 +1,87 @@
+import os
+import uproot
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+import random
+import awkward as ak # Using awkward array for easier handling of jagged data
+import time # For timing steps
+
+# Set seeds for reproducibility
+seed_value = 42
+random.seed(seed_value)
+np.random.seed(seed_value)
+torch.manual_seed(seed_value)
+os.environ['PYTHONHASHSEED'] = str(seed_value)
+
+def load_root_file(file_path, branches=None, print_branches=False):
+    all_branches = {}
+    with uproot.open(file_path) as file:
+        tree = file["tree"]
+        # Load all ROOT branches into array if not specified
+        if branches is None:
+            branches = tree.keys()
+        # Option to print the branch names
+        if print_branches:
+            print("Branches:", tree.keys())
+        # Each branch is added to the dictionary
+        for branch in branches:
+            try:
+                all_branches[branch] = (tree[branch].array(library="np"))
+            except uproot.KeyInFileError as e:
+                print(f"KeyInFileError: {e}")
+        # Number of events in file
+        all_branches['event'] = tree.num_entries
+    return all_branches
+
+branches_list = [
+    't5_innerRadius',
+    't5_bridgeRadius',
+    't5_outerRadius',
+    't5_pt',
+    't5_eta',
+    't5_phi',
+    't5_isFake',
+    't5_t3_idx0',
+    't5_t3_idx1',
+
+    't5_t3_fakeScore1',
+    't5_t3_promptScore1',
+    't5_t3_displacedScore1',
+    't5_t3_fakeScore2',
+    't5_t3_promptScore2',
+    't5_t3_displacedScore2',
+
+    't5_pMatched',
+    't5_sim_vxy',
+    't5_sim_vz',
+    't5_matched_simIdx'
+]
+
+branches_list += [
+    'pLS_eta',
+    'pLS_etaErr',
+    'pLS_phi',
+    'pLS_matched_simIdx',
+    'pLS_circleCenterX',
+    'pLS_circleCenterY',
+    'pLS_circleRadius',
+    'pLS_ptIn',
+    'pLS_ptErr',
+    'pLS_px',
+    'pLS_py',
+    'pLS_pz',
+    'pLS_isQuad',
+    'pLS_isFake'
+]
+
+# Hit-dependent branches
+suffixes = ['r', 'z', 'eta', 'phi', 'layer']
+branches_list += [f't5_t3_{i}_{suffix}' for i in [0, 2, 4] for suffix in suffixes]
+
+file_path = "pls_t5_embed.root"
+branches = load_root_file(file_path, branches_list, print_branches=True)
